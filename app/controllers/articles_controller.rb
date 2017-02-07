f25d5798311d76d9a21057cf86a8f1ce91967580
Kyle Maggard <kjm0001@gmail.com>
2017-02-07 13:43:04 +0000

    updated to allow for editing articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,11 +16,24 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end  
   
   def show
     @article = Article.find(params[:id]) 
   end
-    
+  
+  def edit
+    @article = Article.find(params[:id]) 
+  end
+  
+  def update
+    @article = Article.find(params[:id]) 
+    if @article.update(article_params) 
+      flash[:notice] = "Article was successfully updated."
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
   
   private
