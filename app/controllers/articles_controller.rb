3d13850b22c91564e54eac132fb189c4731047b7
Kyle Maggard <kjm0001@gmail.com>
2017-02-08 13:02:53 +0000

    add partials forms and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,6 @@ class ArticlesController < ApplicationController
   def index
     # Grab all the articles from database
     @articles = Article.all
-    
   end
   
   def new
@@ -45,6 +44,13 @@ class ArticlesController < ApplicationController
       # show edit page again 
       render 'edit'
     end
+  end
+  
+  def destroy
+    @article = Article.find(params[:id]) 
+    @article.destroy
+    flash[:notice]  = "Article was successfully deleted"
+    redirect_to articles_path
   end
   
   private
