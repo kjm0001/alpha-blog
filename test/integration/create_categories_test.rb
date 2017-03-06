077f91b2f05abd4da748670cb8fd2021967cb93e
Kyle Maggard <kjm0001@gmail.com>
2017-03-06 13:28:04 +0000

    Add categories models and tests SEC 7 LECT 136

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,6 +1,5 @@
 require 'test_helper'
 
-
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
   
   test "get new category form and create category" do
@@ -17,7 +16,26 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_template 'categories/index'
     ## and the index page should now have sports displayed in the page
     assert_match "sports", response.body
-      
+  end
+  
+  test "invalid category submission results in failure" do
+    ## going to new category path
+    get new_category_path  
+    ## getting new form
+    assert_template 'categories/new'
+    ## posting to the new form and creating a new category called sports
+    ## 
+    assert_no_difference 'Category.count' do
+      ## no redirection
+      post categories_path, category: {name: " "}
+    end
+    ## redirecting to the new page
+    assert_template 'categories/new'
+    ## looking for the existence of these 2 items
+    ## comes from views=>shared=>_errors.html.erb 
+    ## referencing the errors partial
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
   end
   
   
